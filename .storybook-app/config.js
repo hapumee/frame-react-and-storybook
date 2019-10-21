aea5ae6f7aba719891b86298ef8a58a529041670
youngsunmin <youngsun.min@navercorp.com>
2019-10-21 19:16:28 +0900

    Add README.md

--- .storybook-app/config.js
+++ .storybook-app/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
 configure([
+    require.context('../src/stories', true, /Notice.stories.js/),
     require.context('../src/components', true, /\.stories\.js$/),
     require.context('../src/stories', true, /\.stories\.js$/)
 ], module);
