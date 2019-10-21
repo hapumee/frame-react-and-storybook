9ef7bc5abe5c17b13c45402e121705b133967b83
youngsunmin <youngsun.min@navercorp.com>
2019-10-21 18:46:29 +0900

    Install Storybook for React

---
+++ src/components/ComponentExample.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ComponentExample extends Component {
+    render() {
+        return (
+            <div>
+                <p>Component Example</p>
+                <hr />
+                <p>New that the story 'Welcome' and 'Click Action Example' are demo for usage of storybook,</p>
+                <p>just eliminate those stories.</p>
+            </div>
+        )
+    }
+}
+
+export default ComponentExample;
